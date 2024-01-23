fac21479508aa2fccef24e75d662a215fd1d9e7e
MH.Peng <pmhy27@gmail.com>
2024-01-23 14:06:19 +0800

    complete lab2-2024-01/23-14:05

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -26,7 +26,9 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+            if (firstEqualsLast) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,6 +53,9 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
+        if(lastDigit == 0){
+            return false;
+        }
         while (x > 10) {
             x = x / 10;
         }
@@ -71,11 +76,18 @@ public class IntListExercises {
             return false;
         }
 
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
+        boolean currElemIsPrime = false;
 
-        if (currElemIsPrime) {
-            lst.first *= lst.first;
+        while (lst.rest != null){
+            if (Primes.isPrime(lst.first)){
+                lst.first *= lst.first;
+                currElemIsPrime = true;
+            }
+            lst = lst.rest;
         }
+
+
+
 
         return currElemIsPrime || squarePrimes(lst.rest);
     }
